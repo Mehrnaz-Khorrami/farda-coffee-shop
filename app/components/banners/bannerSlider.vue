c96700a838f8b5d0fe3fcd29623c5546dd926ba1
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-02-28 20:15:03 +0330

    build: banners slider

---
+++ app/components/banners/bannerSlider.vue
@@ -0,0 +1,30 @@
+<script setup>
+const banners = ref([
+  {src: "/img/banners/comboItems.png"},
+  {src: "/img/banners/comboItems.png"},
+  {src: "/img/banners/comboItems.png"},
+])
+</script>
+
+<template>
+  <v-carousel
+      height="140"
+      show-arrows="hover"
+      cycle
+      touch
+      hide-delimiter-background
+  >
+    <template #next="{ props }">
+      <v-btn icon="mdi-chevron-left" variant="text" size="x-small" color="iconColor" @click="props.onClick"></v-btn>
+    </template>
+    <template #prev="{ props }">
+      <v-btn icon="mdi-chevron-right" variant="text" size="x-small" color="iconColor" @click="props.onClick"></v-btn>
+    </template>
+    <v-carousel-item v-for="(item,index) in banners" :key="index">
+      <v-card width="100%" height="140" flat class="rounded-xl" :image="item.src">
+    </v-card>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<style scoped></style>
