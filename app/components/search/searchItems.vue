f9ebc69317f53c6e4ccb43cc647548dde5f0f107
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-02-28 13:29:54 +0330

    feat: search field

---
+++ app/components/search/searchItems.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-text-field density="comfortable" color="primary" variant="solo" flat bg-color="textFieldColorBackground" rounded label="جست و جو...">
+    <template #prepend-inner>
+      <Icon icon="line-md:search" width="2em" height="2em" style="color: rgb(var(--v-theme-iconColor))" />
+    </template>
+  </v-text-field>
+</template>
